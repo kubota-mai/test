0da7c69fd2a2de07066af1bc9ffbbebc11ebb34a
kubota-mai <kubota.s.tennis@gmail.com>
2019-12-25 21:30:00 +0900

    20191225

--- TestProject01/src/HelloWorld.java
+++ TestProject01/src/HelloWorld.java
@@ -3,10 +3,16 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		for(int i=1;i<=100;i++){
-		System.out.println(i);
-		System.out.println("-");
-		}
+		
+		int i=0;
+		do{
+			System.out.println(i);
+			i++;
+		}	while(i<10);
+		
+		
+		
+		
 	}
-
+ 
 }
