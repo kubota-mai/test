12a4245f647baae2779f014e215490b934297857
kubota-mai <kubota.s.tennis@gmail.com>
2020-01-06 21:19:59 +0900

    20200106

---
+++ Rensyu/src/jp/co/internous/action/SetSample.java
@@ -0,0 +1,15 @@
+package jp.co.internous.action;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetSample {
+
+public static void main(String[]args) {
+Set<String>set=new HashSet<String>();
+
+//値のセットはaddメソッドを利用
+
+}
+
+}
