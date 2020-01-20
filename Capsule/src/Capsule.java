7b17da1c200a0b5d64a132aad4f60e60553bc95f
kubota-mai <kubota.s.tennis@gmail.com>
2020-01-20 21:26:03 +0900

    20200120

---
+++ Capsule/src/Capsule.java
@@ -0,0 +1,13 @@
+
+public class Capsule {
+
+	public static void main(String[]args) {
+		Person3 taro=new Person3("山田太郎",20);
+		System.out.println(taro.getName());
+
+		taro.setName("花子");
+
+		System.out.println(taro.getName());
+	}
+
+}
